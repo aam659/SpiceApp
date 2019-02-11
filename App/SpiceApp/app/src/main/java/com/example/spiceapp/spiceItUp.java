2ac037f14cd1a6d0cea8b7c0fa552e736fa3fc86
ldawkins <ldawkins@crimson.ua.edu>
2019-02-10 19:04:04 -0600

    started spice it up activity

---
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/spiceItUp.java
@@ -0,0 +1,13 @@
+package com.example.spiceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class spiceItUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spice_it_up);
+    }
+}
