7223b60fd778d77f22ecc9bd4ae4f5fd3faaa36c
Alan Manning <aamanning@crimson.ua.edu>
2019-02-11 02:18:33 -0600

    Finished styling app website for draft due 2/12/19

--- Website/Js/app.js
+++ Website/Js/app.js
@@ -1,5 +1,5 @@
 /* Search Bar Function */
 document.getElementById('searchform').onsubmit = function() {
-    window.location = "http://www.google.com/search?q=site:" + document.getElementById('search').nodeValue;
+    window.location = "http://www.google.com/search?q=site:http://127.0.0.1:5500/Html" + document.getElementById('search').nodeValue;
     return false;
 }
