8d2cfdce4d7ca19a37bdf51311c1cf0c0e85647f
ldawkins <ldawkins@crimson.ua.edu>
2019-02-15 20:30:07 -0600

    Added a place client and place request to home. I tried SIU page first, but the app kept crashing

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/HomePage.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/HomePage.java
@@ -3,31 +3,91 @@ package com.example.spiceapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.libraries.places.api.Places; //Added by Ryan, Google Places API
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
 import com.google.android.libraries.places.api.net.PlacesClient; //Added by Ryan, Google Places API
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HomePage extends AppCompatActivity {
+//    private PlacesClient placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        //If you put the api key in here, make sure you delete it before you push
+        /*//If you put the api key in here, make sure you delete it before you push
         //If you accidentally push the api key, tell Ryan so he can regenerate the key
         //and redistribute it
-        Places.initialize(getApplicationContext(),"");
+        String apiKey = "AIzaSyDcNwFNunrrjSZQ5HMUhdJKHAVk9CdW59I";
+        Toast.makeText(this, "in create", Toast.LENGTH_LONG).show();
+
+
+        if (apiKey.equals("")) {
+            Toast.makeText(this, "error with api key", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Setup Places Client
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+            placesClient = Places.createClient(this);
+        }*/
     }
 
     public static class HomePageActivity extends AppCompatActivity{
+        private PlacesClient placesClient;
 
 
         protected void onCreate(Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_home_page);
+
+            //If you put the api key in here, make sure you delete it before you push
+            //If you accidentally push the api key, tell Ryan so he can regenerate the key
+            //and redistribute it
+            String apiKey = "AIzaSyDcNwFNunrrjSZQ5HMUhdJKHAVk9CdW59I";
+
+            if (apiKey.equals("")) {
+                Toast.makeText(this, "error with api key", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Setup Places Client
+            if (!Places.isInitialized()) {
+                Places.initialize(getApplicationContext(), apiKey);
+                placesClient = Places.createClient(this);
+            }
+
+            // Define a Place ID.
+            String placeId = "ChIJN1t_tDeuEmsRUsoyG83frY4";
+
+            // Specify the fields to return (in this example all fields are returned).
+            List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME);
+
+            // Construct a request object, passing the place ID and fields array.
+            FetchPlaceRequest request = FetchPlaceRequest.builder(placeId, placeFields).build();
+
+            placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
+                Place place = response.getPlace();
+                Toast.makeText(this, place.getName(), Toast.LENGTH_LONG).show();
+            }).addOnFailureListener((exception) -> {
+                if (exception instanceof ApiException) {
+                    ApiException apiException = (ApiException) exception;
+                    int statusCode = apiException.getStatusCode();
+                    // Handle error with given status code.
+                    Toast.makeText(this, "failed", Toast.LENGTH_LONG).show();
+                }
+            });
 
             final Button btnLogin =(Button) findViewById(R.id.btnLogin);
             final Button btnSpiceItUp =(Button) findViewById(R.id.btnSpice);
