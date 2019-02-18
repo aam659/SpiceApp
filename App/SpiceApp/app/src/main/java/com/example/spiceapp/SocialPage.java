799c826843dd896ea0cf7d6d86ec185db0d18fa0
Ryan Simpson <rnsimpson@crimson.ua.edu>
2019-02-18 16:23:59 -0600

    Created a working app bar, currently only on the home page and the social page. No intention to add to Login page. Will add to SIU and Profile once pages are ready. Issue right now with navigating back to home page

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/SocialPage.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/SocialPage.java
@@ -1,11 +1,14 @@
 package com.example.spiceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class SocialPage extends AppCompatActivity {
@@ -17,14 +20,14 @@ public class SocialPage extends AppCompatActivity {
 
         initializeToolbar();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     private void initializeToolbar(){
@@ -32,6 +35,43 @@ public class SocialPage extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Social");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent nextScreen;
+        switch (item.getItemId()) {
+            case R.id.tlbLogin:
+                nextScreen = new Intent(SocialPage.this, LoginPage.class);
+                startActivityForResult(nextScreen, 0);
+                return true;
+
+            case R.id.tlbSIU:
+                nextScreen = new Intent(SocialPage.this, SpiceItUp.class);
+                startActivityForResult(nextScreen, 0);
+                return true;
+
+            case R.id.tlbProfile:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.tlbHome:
+                nextScreen = new Intent(SocialPage.this, HomePage.class);
+                startActivityForResult(nextScreen, 0);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 }
