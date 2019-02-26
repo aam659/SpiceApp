fbcfab296fc08bc12a12f6b424ec02955870fdce
Ryan Simpson <rnsimpson@crimson.ua.edu>
2019-02-25 22:57:55 -0600

    Reworked login/register page

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/RegisterPage.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/RegisterPage.java
@@ -1,24 +1,28 @@
 package com.example.spiceapp;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.transition.Fade;
+import android.view.View;
+import android.view.Window;
 
 public class RegisterPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        getWindow().setSharedElementEnterTransition(new Fade());
         setContentView(R.layout.activity_register_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        initializeToolbar();
     }
 
-    private void initializeToolbar(){
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Register");
-    }
 }
