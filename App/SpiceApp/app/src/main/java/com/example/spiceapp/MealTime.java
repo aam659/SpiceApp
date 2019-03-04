3065d48b98f3cd7901faa54260e762df60ca6434
Ryan Simpson <rnsimpson@crimson.ua.edu>
2019-03-03 19:57:50 -0600

    no message

---
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/MealTime.java
@@ -0,0 +1,14 @@
+package com.example.spiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_time);
+    }
+}
