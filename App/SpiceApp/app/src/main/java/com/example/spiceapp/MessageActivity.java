12607575a319a416aea2c5d0751c9e37727f38a2
Ryan Simpson <rnsimpson@crimson.ua.edu>
2019-04-11 16:23:51 -0500

    Can send a message to the database, VERY LIMITED in functionality (only one chat can exist at a time, no chat history show in app)

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/MessageActivity.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/MessageActivity.java
@@ -9,7 +9,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.spiceapp.FirebaseObjects.User;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,9 +30,11 @@ public class MessageActivity extends AppCompatActivity {
 
     CircleImageView profile_pic;
     TextView userName;
-
+    FirebaseUser mUser;
     DatabaseReference reference;
     Intent intent;
+    EditText message_send;
+    ImageButton send;
 
 
     @Override
@@ -36,8 +42,10 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
         initializeToolbar();
+        mUser = FirebaseManager.getCurrentUser();
         profile_pic = findViewById(R.id.profileImg);
-        userName = findViewById(R.id.contactName);
+        message_send = findViewById(R.id.message_send);
+        send = findViewById(R.id.btnSend);
         intent = getIntent();
         String userEmail = intent.getStringExtra("userEmail");
         reference = FirebaseDatabase.getInstance().getReference("users")
@@ -59,6 +67,18 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!message_send.equals("")){
+                    sendMessage(mUser.getEmail().replace('.','_'), userEmail, message_send.getText().toString());
+                }
+                else
+                    Toast.makeText(MessageActivity.this, "Message empty.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     private void initializeToolbar(){
@@ -73,6 +93,8 @@ public class MessageActivity extends AppCompatActivity {
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("sender", sender);
         hashMap.put("reciever", reciever);
+        hashMap.put("message", message);
+        reference.child("Chats").setValue(hashMap);
 
         //FINISH HASHMAP
 
