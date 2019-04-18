1658a3e13ea2f84443aadc57c5c8516d5ee8561b
Donald <dlochbil@umich.edu>
2019-04-18 15:00:42 -0500

    populate edit info data

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/AdditionalDetails.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/AdditionalDetails.java
@@ -1,24 +1,30 @@
 package com.example.spiceapp;
 
+import afu.org.checkerframework.checker.nullness.qual.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-
-//TODO: POPULATE VIEWS IF USER IS EDITING
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdditionalDetails extends AppCompatActivity {
 
     private DatabaseReference database;
-
+    private String fname;
+    private String lname;
+    private String phoneNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,34 @@ public class AdditionalDetails extends AppCompatActivity {
         TextView phoneNumber = (TextView) findViewById(R.id.edtPhone);
         TextView lName = (TextView) findViewById(R.id.edtLName);
 
+        /*Functionality to display the user info that is currently stored in the database
+            The text boxes used to edit will be populated with
+            First Name, Last Name, and Phone Number
+         */
+                Query query = FirebaseManager.getDatabaseReference().child("users").child(FirebaseManager.getCurrentUser().getEmail().replace('.','_'));
+                    /*
+                    Queries database for current user mood
+                     */
+                query.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        fname = dataSnapshot.child("fName").getValue(String.class);
+                        lname = dataSnapshot.child("lName").getValue(String.class);
+                        phoneNum = dataSnapshot.child("phoneNumber").getValue(String.class);
+                        name.setText(fname);
+                        lName.setText(lname);
+                        phoneNumber.setText(phoneNum);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+                /*When Finish button is clicked save all the text views data
+                    This is used to update the user information
+                 */
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +80,7 @@ public class AdditionalDetails extends AppCompatActivity {
         });
 
     }
+            //After the information is saved it is uploaded to firebase
 
     protected void addDetails(String fName, String phone, String lName, FirebaseUser user) {
         String email = getIntent().getStringExtra("UserEmail");
