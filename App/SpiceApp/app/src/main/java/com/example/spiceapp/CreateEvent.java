08e50fcba2e18b7b984aa8faa01d3bce2fbd89f3
logan064 <dawkins.logan@gmail.com>
2019-04-20 00:25:02 -0500

    added place to event

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/CreateEvent.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/CreateEvent.java
@@ -29,15 +29,20 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.MapEngine;
 import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.search.Address;
 import com.here.android.mpa.search.DiscoveryResult;
 import com.here.android.mpa.search.DiscoveryResultPage;
 import com.here.android.mpa.search.ErrorCode;
+import com.here.android.mpa.search.Place;
 import com.here.android.mpa.search.PlaceLink;
+import com.here.android.mpa.search.PlaceRequest;
 import com.here.android.mpa.search.ResultListener;
+import com.here.android.mpa.search.ReverseGeocodeRequest;
 import com.here.android.mpa.search.SearchRequest;
 
 import java.util.ArrayList;
@@ -63,7 +68,6 @@ public class CreateEvent extends AppCompatActivity {
     private double deviceLongitude;
     private Mood mood;
     private int distance;
-    private PlaceLink place;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,12 +140,8 @@ public class CreateEvent extends AppCompatActivity {
                     if (!dataSnapshot.child(eventName).exists()) {
                         mEmails = adapter.getCheckedUsers();
                         if(mEmails.size() > 1){
-                            createMoodList(eventName, new PlaceCallBack() {
-                                @Override
-                                public void onCallback() {
-                                    //findPlace();
-                                }
-                            });
+                            createMoodList(eventName);
+                            findPlace();
                             reference.child(eventName).child("users").setValue(mEmails);
                             reference.child(eventName).child("eventName").setValue(eventName);
                             reference.child(eventName).child("rsvp").setValue(getHashMap());
@@ -177,7 +177,7 @@ public class CreateEvent extends AppCompatActivity {
         return rsvp;
     }
 
-    private void createMoodList(String eventName,CreateEvent.PlaceCallBack placeCallBack){
+    private void createMoodList(String eventName){
         for(String email : mEmails){
             DatabaseReference currentPreferenceReference = FirebaseDatabase.getInstance().getReference("users").child(email).child("CurrentPreference");
             currentPreferenceReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -197,12 +197,8 @@ public class CreateEvent extends AppCompatActivity {
                 }
             });
         }
-        placeCallBack.onCallback();
     }
 
-    private interface PlaceCallBack{
-        void onCallback();
-    }
 
     /**
      * initMapEngine
@@ -230,34 +226,47 @@ public class CreateEvent extends AppCompatActivity {
      * if signed in it uses current mood else it uses a random search
      */
     private void findPlace(){
+        String eventName = editEventName.getText().toString();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Events").child(eventName);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<ArrayList<Mood>> t = new GenericTypeIndicator<ArrayList<Mood>>() {};
+                ArrayList<Mood> yourStringArray = dataSnapshot.child("currentPreferences").getValue(t);
+                if(currentPreferences.size() > 0) mood = currentPreferences.get(0);
+                ArrayList<String> categories = mood.getCategories();
+
+
+                distance = (int) mood.getDistance();
+                String query = "";
+                if (categories != null) {
+                    for (int i = categories.size() - 1; i > -1; --i) {
+                        if (i != -1)
+                            query += ", ";
+
+                        query += categories.get(i);
+                    }
+                } else {
+                    query += ", Breakfast";
+                }
+
+                SearchRequest searchRequest;
+                searchRequest = new SearchRequest("Restaurant" + query );
+
+                searchRequest.setSearchCenter(new GeoCoordinate(deviceLatitude,deviceLongitude));
+
+                // Checks device coordinates
+                System.out.println("COORDINATES: " + String.valueOf(deviceLatitude) + " " + String.valueOf(deviceLongitude));
+
+                searchRequest.execute(discoveryResultPageListener);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
         //Random rand = new Random();
         //rand.nextInt(currentPreferences.size())
-        mood = currentPreferences.get(0);
-        ArrayList<String> categories = mood.getCategories();
 
-
-        distance = (int) mood.getDistance();
-        String query = "";
-        if (categories != null) {
-            for (int i = categories.size() - 1; i > -1; --i) {
-                if (i != -1)
-                    query += ", ";
-
-                query += categories.get(i);
-            }
-        } else {
-            query += ", Breakfast";
-        }
-
-        SearchRequest searchRequest;
-        searchRequest = new SearchRequest("Restaurant" + query );
-
-        searchRequest.setSearchCenter(new GeoCoordinate(deviceLatitude,deviceLongitude));
-
-        // Checks device coordinates
-        System.out.println("COORDINATES: " + String.valueOf(deviceLatitude) + " " + String.valueOf(deviceLongitude));
-
-        searchRequest.execute(discoveryResultPageListener);
     }
 
     /**
@@ -279,10 +288,12 @@ public class CreateEvent extends AppCompatActivity {
                 for (DiscoveryResult item : s_ResultList) {
                     if (item.getResultType() == DiscoveryResult.ResultType.PLACE) {
                         PlaceLink placeLink = (PlaceLink) item;
-
                         // Check for distance
                         if ((placeLink.getDistance() * .00062137) <= distance) {
-                            place = placeLink;
+                            PlaceRequest placeRequest = placeLink.getDetailsRequest();
+                            System.out.println("Distance: " + (placeLink.getDistance() * .00062137));
+                            System.out.println("PLACEDETS" + placeRequest.getContent().toString());
+                            placeRequest.execute(m_placeResultListener);
                             break;
                         }
                     }
@@ -295,6 +306,50 @@ public class CreateEvent extends AppCompatActivity {
             }
         }
     };
+
+    /**
+     * result listener for place request in discovery listener
+     */
+    private ResultListener<Place> m_placeResultListener = new ResultListener<Place>() {
+        @Override
+        public void onCompleted(Place place, ErrorCode errorCode) {
+            if (errorCode == ErrorCode.NONE) {
+                // Check for place coordinates
+                System.out.println("GEOCOORD" + String.valueOf(place.getLocation().getCoordinate()));
+                getHereAddress(place.getLocation().getCoordinate(),place.getName());
+            }
+            else {
+                Toast.makeText(getApplicationContext(),
+                        "ERROR:Place request returns error: " + errorCode, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    };
+
+    /**
+     * getHereAddress
+     * @param geoCoordinate
+     * retrieves the restaurants address
+     */
+    private void getHereAddress(GeoCoordinate geoCoordinate,String name){
+        String eventName = editEventName.getText().toString();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Events").child(eventName);
+        ReverseGeocodeRequest revGeo = new ReverseGeocodeRequest(geoCoordinate);
+        revGeo.execute((new ResultListener<Address>() {
+            @Override
+            public void onCompleted(Address address, ErrorCode errorCode) {
+                if(errorCode == ErrorCode.NONE){
+                    ref.child("name").setValue(name);
+                    ref.child("addr").setValue(address.getText());
+                    ref.child("lat").setValue(geoCoordinate.getLatitude());
+                    ref.child("lon").setValue(geoCoordinate.getLongitude());
+                }
+                else{
+                    System.out.println("Failed");
+                }
+            }
+        }));
+    }
 
     /**onRequestPermissionsResult for location permission
      *
