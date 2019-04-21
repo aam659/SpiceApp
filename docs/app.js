34e5396c72f8e5cfab7609068bbed718ca1fed68
Alan Manning <aamanning@crimson.ua.edu>
2019-04-21 04:30:56 -0500

    Working on Website - search bar now works, but only for actual domains

--- docs/app.js
+++ docs/app.js
@@ -1,28 +1,13 @@
 /* Search Bar Function */
 
-//Enter domain of site to search.
-let domainroot="https://aam659.github.io/SpiceApp/docs/downloads.html";
+// Enter domain of site to search.
+// Only works if actual domain name
+let domainroot="https://aam659.github.io/SpiceApp/";
 
-let searchaction=[ //form action for the 3 search engines
-    "http://www.google.com/search",
-    "http://search.yahoo.com/search",
-    "http://search.msn.com/results.aspx"
-]
+// name of hidden query form for the 3 search engines
+let queryfieldname="q";
 
-let queryfieldname=["q","p","q"] //name of hidden query form for the 3 search engines
-
-function switchaction(cur, index){
-    cur.form.action=searchaction[index];
-    document.getElementById("hiddenquery").name=queryfieldname[index];
-}
-
-function jksitesearch(curobj){
-    for (i=0; i< document.jksearch.se.length; i++){ //loop through radio to see which is checked
-
-        if (document.jksearch.se[i].checked==true)
-            switchaction(document.jksearch.se[i], i);
-    }
-    
-    document.getElementById("hiddenquery").value="site:"+domainroot+curobj.qfront.value;
+function sitesearch(curobj){
+    document.getElementById("hiddenquery").value="site:"+domainroot+ " " + curobj.qfront.value;
 }
 
