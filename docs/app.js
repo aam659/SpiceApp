1324f36f44172456244e282b2b1799358fd25afb
Alan Manning <aamanning@crimson.ua.edu>
2019-04-21 03:30:45 -0500

    Working on website

--- docs/app.js
+++ docs/app.js
@@ -16,17 +16,17 @@ let searchaction=[ //form action for the 3 search engines
 let queryfieldname=["q","p","q"] //name of hidden query form for the 3 search engines
 
 function switchaction(cur, index){
-    cur.form.action=searchaction[index]
-    document.getElementById("hiddenquery").name=queryfieldname[index]
+    cur.form.action=searchaction[index];
+    document.getElementById("hiddenquery").name=queryfieldname[index];
 }
 
 function jksitesearch(curobj){
     for (i=0; i< document.jksearch.se.length; i++){ //loop through radio to see which is checked
 
         if (document.jksearch.se[i].checked==true)
-            switchaction(document.jksearch.se[i], i)
+            switchaction(document.jksearch.se[i], i);
     }
     
-    document.getElementById("hiddenquery").value="site:"+domainroot+" "+curobj.qfront.value
+    document.getElementById("hiddenquery").value="site:"+domainroot+curobj.qfront.value;
 }
 
