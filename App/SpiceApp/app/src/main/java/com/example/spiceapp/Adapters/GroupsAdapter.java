105a33e86a71de4f6962def67d82010e68b0a183
Ryan Simpson <rnsimpson@crimson.ua.edu>
2019-04-22 17:42:33 -0500

    Added images to groups

--- App/SpiceApp/app/src/main/java/com/example/spiceapp/Adapters/GroupsAdapter.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/Adapters/GroupsAdapter.java
@@ -2,6 +2,7 @@ package com.example.spiceapp.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,12 @@ import com.example.spiceapp.FirebaseObjects.User;
 import com.example.spiceapp.GroupMessageActivity;
 import com.example.spiceapp.MessageActivity;
 import com.example.spiceapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,6 +30,11 @@ public class GroupsAdapter extends RecyclerView.Adapter<GroupsAdapter.ViewHolder
 
     private Context mContext;
     private List<Group> mGroups;
+    private StorageReference storageReference;
+    private FirebaseStorage storage;
+
+
+
 
     public GroupsAdapter(Context mContext, List<Group> mGroups){
         this.mContext = mContext;
@@ -42,7 +54,22 @@ public class GroupsAdapter extends RecyclerView.Adapter<GroupsAdapter.ViewHolder
         System.out.println("GROUP NAME " + group.getGroupName());
         holder.groupName.setText(group.getGroupName());
 
-        //TODO: Somehow add profile picture
+        // add profile picture
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        Task<Uri> task = storageReference.child("images/"+group.getGroupName()).getDownloadUrl();
+        task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(holder.profileImage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@androidx.annotation.NonNull Exception e) {
+                System.out.println("Failed to load pic");
+            }
+        });
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
--- App/SpiceApp/app/src/main/java/com/example/spiceapp/ChooseChatPicture.java
+++ App/SpiceApp/app/src/main/java/com/example/spiceapp/ChooseChatPicture.java
@@ -1,14 +1,154 @@
 package com.example.spiceapp;
 
+import afu.org.checkerframework.checker.nullness.qual.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class ChooseChatPicture extends AppCompatActivity {
+
+    private final int PICK_IMAGE_REQUEST = 71;
+
+    private CardView btnConfirm;
+    private ImageView btnImage;
+
+    String groupName;
+    private FirebaseStorage storage;
+    StorageReference storageReference;
+    private Uri filePath;
+    private final String TAG = "ProfilePage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_chat_picture);
+        groupName = getIntent().getStringExtra("groupName");
+
+        btnConfirm = (CardView) findViewById(R.id.btnMakeEvent);
+        btnImage = (ImageView) findViewById(R.id.imgGroup);
+
+        FirebaseApp.initializeApp(getApplicationContext());
+        FirebaseManager.initialize();
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GroupMessageActivity.class);
+                intent.putExtra("groupName", groupName);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+                    } else {
+
+                        getPhoto();
+
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+            System.out.println(groupName);
+            StorageReference ref = storageReference.child("images/"+ groupName);
+            System.out.println("File name:" + FirebaseManager.getCurrentUser().getEmail().replace('.','_'));
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ChooseChatPicture.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ChooseChatPicture.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            System.out.println("Error message: " + e.getMessage());
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            filePath = data.getData();
+
+            try {
+
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), filePath);
+
+                ImageView imageView = (ImageView) findViewById(R.id.imgGroup);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            uploadImage();
+        }
+    }
+
+    public void getPhoto() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
 }
